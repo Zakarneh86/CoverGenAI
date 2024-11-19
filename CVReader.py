cee1976c071fe4df2683bed880d9e18576d865ef
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-11-19 11:47:27 +0000

    Adding API Key and getting CV Summary

--- CVReader.py
+++ CVReader.py
@@ -7,13 +7,43 @@ from openai import OpenAI
 
 class CVReader:
 
-    def __init__(self):
+    def __init__(self, openAiKey):
         self.cvText = ''
         self.coverText = ''
+        try:
+            self.client = OpenAI(openAiKey)
+            self.ClientConnected = True
+        except:
+            self.ClientConnected = False
+        
 
-    def getCVText(self, cvFile):
-        doc = fitz.open(stream = cvFile)
+    def getCvText(self, cvFile):
+        doc = fitz.open(stream = 
+                        cvFile)
         for i in range(doc.page_count):
             page = doc.load_page(i)
             self.cvText += page.get_text()
         return self.cvText
+
+    def getCvSummary(self):
+        prompt = "Provide a detailed summary, focusing on Education, Experience, Achievments, Certification and Projects: "
+        prompt = prompt + self.coverText
+
+        message = [{"role": "user", "content": f"{prompt}"}]
+
+        if self.ClientConnected:
+            try:
+                response = self.client.chat.completions.create(
+                    model = "gpt-4",
+                    messages= message,
+                    max_tokens= 500,
+                    temperature = 0.8)
+                gotResponse = True
+            except:
+                gotResponse = False
+
+        if gotResponse:
+            summary = response.choices[0].message.content
+        else:
+            summary = None
+        return summary, gotResponse
--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import CVReader
 
-cvreader = CVReader.CVReader()
+apiKeys = st.secrets["API_Keys"]
+openAiKey = apiKeys["openAI"]
+
+cvreader = CVReader.CVReader(openAiKey)
+
 
 uploaded_cv = st.file_uploader("Upload CV")
 
 if uploaded_cv is not None:
-    cvText = cvreader.getCVText(uploaded_cv.read())
+    cvText = cvreader.getCvText(uploaded_cv.read())
 
-    st.write(cvText)
+    summary, gotResponse = cvreader.getCvSummary()
+    st.write(summary)
+
