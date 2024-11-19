8190e189f110a9e627d4a6c899b81c101994da94
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-11-19 12:57:07 +0000

    adding getCoverLetter Method

--- CVReader.py
+++ CVReader.py
@@ -48,3 +48,20 @@ class CVReader:
             else:
                 self.summary = None
         return self.summary, gotResponse
+    
+    def getCoverLetter(self, employerName, jobTitle, recruiterName, jobDescription):
+        cover_prompt = f'''Given the below resume summary:
+              {self.summary}
+              and the below job description:
+              {jobDescription}
+              write a job application cover letter. The company name is 
+              {employerName} and the job title is {jobTitle}.
+                And the recruiter name is {recruiterName}'''
+        messages = [
+        {"role": "user", "content": f"{cover_prompt}"}]
+        response = self.client.chat.completions.create(model="gpt-4",
+                                    messages=messages,
+                                    max_tokens=500,
+                                    temperature=0.8)
+        coverLetter = response.choices[0].message.content
+        return coverLetter
