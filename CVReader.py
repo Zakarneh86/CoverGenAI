b74d35d54c6b176a21162fa4b31819e5824b929a
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-11-19 08:38:46 +0000

    Initial Setup

--- CVReader.py
+++ CVReader.py
@@ -1,1 +1,19 @@
-import PyMuPDF
+import fitz
+import json
+import openai
+import os
+from openai import OpenAI
+
+
+class CVReader:
+
+    def __init__(self):
+        self.cvText = ''
+        self.coverText = ''
+
+    def getCVText(self, cvFile):
+        doc = fitz.open(cvFile)
+        for i in range(doc.page_count):
+            page = doc.load_page(i)
+            self.cvText += page.get_text()
+        return self.getCVText
--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import CVReader
 
+cvreader = CVReader.CVReader()
+
+uploaded_cv = st.file_uploader("Upload CV")
+
+if uploaded_cv is not None:
+    cvText = cvreader.getCVText(uploaded_cv)
+
+    st.write(cvText)
