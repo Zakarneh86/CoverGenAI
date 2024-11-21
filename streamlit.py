9a6661f2848c6afbc66fcfe54aa6c4171c967a23
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-11-21 08:19:24 +0000

    1

--- streamlit.py
+++ streamlit.py
@@ -11,8 +11,9 @@ uploaded_cv = st.file_uploader("Upload CV")
 if uploaded_cv is not None:
     cvText = cvreader.getCvText(uploaded_cv.read())
     summary= cvreader.getCvSummary()
+    badClient = cvreader.badClient
     
-    if not cvreader.badClient:
+    if not badClient:
         employerName = st.text_input("Employer Name", "Employer Name")
         jobTitle = st.text_input("Job Title", "Job Title")
         recruiterName = st.text_input("Recruiter Name", "Recruiter")
@@ -25,7 +26,8 @@ if uploaded_cv is not None:
                     if recruiterName !="":
                         if jobDescription != "Paste Job Description" and jobDescription != "":
                             coverLetter = cvreader.getCoverLetter(employerName, jobTitle, recruiterName, jobDescription)
-                            if not cvreader.badClient:
+                            badClient = cvreader.badClient
+                            if not badClient:
                                 st.write(coverLetter)
                             else:
                                 st.write(cvreader.clientError)
