5ef0ce3b8fb87ca72997f2e9a0c606a58d7840f4
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2024-11-26 23:05:06 +0000

    9

--- CVReader.py
+++ CVReader.py
@@ -141,7 +141,7 @@ class CVReader:
 
             # Inserting Left Bar Text (Name and Title)
             page.insert_textbox(
-                pymupdf.Rect(19, 24, left_bar_width - 5, 150),
+                pymupdf.Rect(19, 25, left_bar_width - 5, 50),
                 userName,
                 fontsize=sideFontSize,
                 fontname=sideFontName,
@@ -149,7 +149,7 @@ class CVReader:
                 align=0  # Left align
             )
             page.insert_textbox(
-                pymupdf.Rect(19, 155, left_bar_width - 5, 200),
+                pymupdf.Rect(19, 55, left_bar_width - 5, 80),
                 usereMail,
                 fontsize=16,  # Slightly smaller title
                 fontname=sideFontName,
@@ -157,7 +157,7 @@ class CVReader:
                 align=0  # Left align
             )
             page.insert_textbox(
-            pymupdf.Rect(19, 205, left_bar_width - 5, 300),
+            pymupdf.Rect(19, 85, left_bar_width - 5, 110),
             userPhone,
             fontsize=16,  # Slightly smaller title
             fontname=sideFontName,
